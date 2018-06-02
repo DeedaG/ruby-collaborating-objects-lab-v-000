876516ae35fc91ef6b2b566639f6da401eb12e0f
Christia T <christiatidwell@bellsouth.net>
2018-06-02 04:03:45 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,7 +17,7 @@ class MP3Importer
   end
 
   def import
-    self.files.include?(Song.new) do |filename|
+    self.files.fill do |filename|
       #binding.pry
      Artist.all << Song.new_by_filename(filename)
     end
